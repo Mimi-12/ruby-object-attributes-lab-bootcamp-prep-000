dddb3253205d7a51db6322131efafeb54b89efd4
selam lemma <oneiconforlife26@gmail.com>
2020-09-30 00:03:34 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,12 @@
+  class Dog
+      def initialize (dog_name,breed)
+       @z_name = dog_name
+       @z_breed = breed
+       end
+      def name
+        @z_name = name
+       end
+       end
+     def breed
+        @z_breed = name
+   end
